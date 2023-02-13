2dae582200521706feffe87f802f814bacb61964
Antony Fernando <89105412+AntonyFernando3@users.noreply.github.com>
2023-02-13 14:09:11 -0300

    Add files via upload

---
+++ Exemplos/exemplo.php
@@ -0,0 +1,21 @@
+<?php
+$mensagem1 = "Boa Tarde";
+$mensagem2 = "Turma ADS";
+$soma = 1 + 2;
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo de PHP</title>
+</head>
+<body>
+    <?php 
+  echo $mensagem1;
+  echo $soma;
+    ?>
+</body>
+</html>
---
+++ Exemplos/exemplo1.php
@@ -0,0 +1,11 @@
+<?php
+$var1 = 1; 
+
+echo $var1; //exibe o valor da variavel
+//exibe o texto (Ola) e valor da variavel
+
+echo "Olá, $var1"; //substitui pelo valor da variavel
+echo 'Olá, $var1'; //exibe o valor da variavel
+echo "Olá," .$var1; // ponto para concatenar
+echo 'Olá,' .$var1;
+?>
