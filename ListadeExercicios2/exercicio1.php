f04e6028fa7016d2b637a82f2be1133bbb79eefd
Antony Fernando <89105412+AntonyFernando3@users.noreply.github.com>
2023-02-16 17:58:46 -0300

    Add files via upload

---
+++ ListadeExercicios2/exercicio1.php
@@ -0,0 +1,25 @@
+<?php
+
+$num = $_GET['num'];
+for ($i = 1; $i <= 10; $i++){
+ echo $num * $i;
+ echo "<br>";
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>tabuada</title>
+
+    <br>
+    <h1> Para exibir a tabuada, digite no final da URL: num?="numero" sem as aspas</h1>
+</head>
+<body>
+    
+</body>
+</html>
